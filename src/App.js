708ddec0a0721bee257ba8b53251554a3aebcc92
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-06-08 12:42:09 -0700

    had to re-make my project since files were in  the wrong versions of apps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
-function App() {
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import userService from './utils/userService';
+import SignupPage from './pages/SignupPage/SignupPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import  "./style.css"
+import './App.css'
+// import ShowPage from './pages/ShowPage/ShowPage';
+
+import tokenService from './utils/tokenService';
+
+
+///////////////// AUTH
+
+
+
+
+//////////////////
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    // Initialize user if there's a token, otherwise null
+      user: userService.getUser(),
+      exercises: []
+
+    };
+  }
+
+  getExerciseById = (id) => {
+    return (
+    this.state.exercises.find(e => e._id===id)
+    )
+    
+  }
+
+
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar user = {this.state.user} handleLogout = {this.handleLogout} />
+      <div className="container">
+      {/* <Navbar user = {this.state.user} handleLogout = {this.handleLogout} /> */}
+      <br/>
+      <section>
+      <Route exact path="/" render={(props) => (
+            <ExercisesList
+              //passing user from app to home so i can access it in NavBar!! important!!
+              user={this.state.user}
+              handleLogout={this.handleLogout}
+              {...props} />
+              )}/>
+                {/* <Route exact path="/exercise/:id" render={(props) => (
+            <ShowPage
+            getExerciseById = {this.getExerciseById}
+              {...props} />
+              )}/> */}
+      </section>
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      <Route exact path='/signup' render={({ history }) => 
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+      <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+      </div>
+      
+    </Router>
   );
+   }
 }
 
 export default App;
---
+++ src/components/navbar.component.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = (props) => {
+    const nav = props.user ?
+        <div class="navity">
+            
+            
+            <Link to="/create" className="nav-link">Create Exercise Log</Link>
+            
+            <Link to="/" className="nav-link">Exercises</Link>
+            
+            <Link to=''className="nav-link" onClick={props.handleLogout}>Log out</Link>
+           
+            <span className='nav-link'>WELCOME, {props.user.name}</span>
+            
+        </div>
+        :
+        <div className="nav">
+            <Link to="/create" className="nav-link">Create Exercise Log</Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link to="/" className="nav-link">Exercises</Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link to='/login' className="nav-link">Log in</Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link to='/signup' className="nav-link">Sign up</Link>
+            
+        </div>
+    return (
+        <div className='NavBar'>
+            {nav}
+        </div>
+    )}
+export default NavBar;
